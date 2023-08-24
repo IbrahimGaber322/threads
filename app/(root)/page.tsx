3b08873f661b73417117c25c65889a4d2fe21064
IbrahimGaber322 <ibrahimseda322@gmail.com>
2023-08-24 18:45:01 +0300

    home if not user

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,11 @@
 import ThreadCard from "@/components/cards/ThreadCard";
 import { fetchThreads } from "@/lib/actions/thread.actions";
 import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
   const user = await currentUser();
+  if(!user) redirect('/sign-in');
 
   const result = await fetchThreads(1, 30);
 
